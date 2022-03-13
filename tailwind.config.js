ace7c4b51b471b0fc2b9d36b7e1461b525f56a4b
Andy Williams <andy@nonissue.org>
2022-03-12 21:27:56 -0700

    remove radix ui colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 /* eslint-disable */
-
-const radixDark = require("./plugins/radix-colors/dark");
-const radixLight = require("./plugins/radix-colors/light");
-const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
@@ -18,35 +14,9 @@ module.exports = {
       colors: {
         transparent: "transparent",
         current: "currentColor",
-        yellow: colors.yellow,
-        purple: colors.purple,
-        blue: colors.blue,
-        gray: colors.gray,
-        bluegray: colors.blueGray,
         almostblack: "hsl(221, 30%, 7%)",
-        red: colors.red,
-
-        darkRadixRed: radixDark.red,
-        lightRadixRed: radixLight.red,
-        darkRadixTomato: radixDark.tomato,
-        lightRadixTomato: radixLight.tomato,
-        orange: colors.orange,
-        green: colors.green,
-        pink: colors.pink,
-        fuchsia: colors.fuchsia,
-        indigo: colors.indigo,
-        lime: colors.lime,
-        amber: colors.amber,
-        sky: colors.sky,
         white: "#fff",
-        mint: radixDark.mint,
-        lime: radixDark.lime,
-        limeLight: radixLight.lime,
-        limeA: radixDark.limeA,
-        amber: radixDark.amber,
-        crimson: radixDark.crimson,
       },
-
       fontSize: {
         xxs: ".625rem",
       },
@@ -92,32 +62,25 @@ module.exports = {
         // Reference custom fontFamily: https://github.com/tailwindlabs/tailwindcss-typography/issues/14
         DEFAULT: {
           css: {
-            color: theme("colors.gray.800"),
+            // color: theme("colors.slate.400"),
             a: {
               transition: "all 0.15s ease-in",
-              color: theme("colors.lightRadixRed.red11"),
-              background: theme("colors.lightRadixRed.red4"),
+
               lineHeight: `1.7`,
               display: "inline-block",
               "text-decoration": "none",
-              "&:hover, &.active": {
-                color: theme("colors.lightRadixRed.red12"),
-                background: theme("colors.lightRadixRed.red3"),
-              },
+              "&:hover, &.active": {},
               "text-decoration": "none",
               paddingRight: `${theme("padding[1]")}`,
               paddingLeft: `${theme("padding[1]")}`,
-              // paddingRight: `calc(${theme("padding[0.5]")} * 3)`,
-              // paddingLeft: `calc(${theme("padding[0.5]")} * 3)`,
               fontWeight: "false",
               display: "inline-block",
             },
-            h1: { color: theme("colors.gray.800") },
+            h1: { color: theme("colors.slate.300") },
             h2: {
               // color: theme("colors.gray.800"),
-              color: theme("colors.lightRadixTomato.tomato9"),
               fontFamily: `${theme("fontFamily.sans")}`,
-              fontWeight: 900,
+              fontWeight: 700,
             },
             h3: {
               color: theme("colors.gray.600"),
@@ -136,9 +99,7 @@ module.exports = {
                 display: "inline",
                 padding: `0px !important`,
                 background: `${theme("colors.transparent")} !important`,
-                "&:hover, &.active": {
-                  color: theme("colors.lightRadixRed.red12"),
-                },
+                "&:hover, &.active": {},
               },
             },
             code: {
@@ -155,21 +116,16 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme("colors.gray.900"),
+            // color: theme("colors.gray.900"),
             a: {
-              color: theme("colors.lightRadixRed.red6"),
-              background: theme("colors.darkRadixRed.red4"),
               "text-decoration": "none",
-              "&:hover, &.active": {
-                color: theme("colors.lightRadixRed.red3"),
-                background: theme("colors.darkRadixRed.red7"),
-              },
+              "&:hover, &.active": {},
             },
             h1: {
               color: theme("colors.white"),
             },
             h2: {
-              color: theme("colors.red.100"),
+              color: theme("colors.blue.100"),
             },
             h3: {
               color: theme("colors.gray.200"),
@@ -186,9 +142,7 @@ module.exports = {
                 // display: "inline-block",
                 background: `${theme("colors.transparent")} !important`,
 
-                "&:hover, &.active": {
-                  color: theme("colors.darkRadixRed.red12"),
-                },
+                "&:hover, &.active": {},
               },
             },
             code: {
@@ -204,19 +158,6 @@ module.exports = {
           },
         },
       }),
-    },
-  },
-  variants: {
-    extend: {
-      backgroundImage: ["hover", "dark"],
-      backgroundOpacity: ["dark"],
-      borderWidth: ["dark", "last"],
-      margin: ["first"],
-      padding: ["last"],
-      ringColor: ["dark", "hover"],
-      ringOpacity: ["dark", "hover"],
-      ringWidth: ["dark", "hover"],
-      typography: ["dark"],
     },
   },
   plugins: [
