b77308b708525fbbbe45175319b4e76c219f9d7b
Andy Williams <andy@nonissue.org>
2022-03-12 21:28:43 -0700

    add credit to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,21 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="z-0 bottom-0 static flex flex-col  justify-start  px-6 md:px-8 max-w-3xl mx-auto mb-8 ">
-      <div className="font-mono font-light leading-relaxed border-0 border-gray-300 bg-opacity-100 text-almostblack opacity-50 dark:text-gray-50 dark:border-gray-600 ">
-        <span
+    <div className="z-0 bottom-0 static flex flex-col justify-start px-6 md:px-8 max-w-3xl mx-auto mb-8">
+      <div className="font-sans font-light leading-relaxed border-0 border-gray-300 bg-opacity-100 text-almostblack opacity-25 hover:opacity-75 transition-opacity duration-300 dark:text-gray-50 dark:border-gray-600  text-center ">
+        {/* <span
           role="img"
           aria-label="100 emoji"
           className="h-5 leading-5 text-sm inline-block"
         >
           © coleandmel.com
+        </span> */}
+        <span className="text-xs">
+          site by{" "}
+          <Link href="https://andy.ws">
+            <a className="underline font-semibold">andy</a>
+          </Link>
         </span>
       </div>
     </div>
