9fc5bce2a0f9e23999db4778af9fe5463c1fc27d
Andy Williams <andy@nonissue.org>
2022-03-13 01:00:09 -0700

    fix typescript / eslint explicit-module-boundary-types error (upgrading typescript-eslint plugin fixed it?)

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,15 @@
-export type Attendee = {
-  name: string;
-  plusOne: boolean;
+import type { NextComponentType, NextPageContext } from "next";
+import type { AppProps } from "next/app";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PageWithLayout<P> = NextComponentType<NextPageContext, any, P> & {
+  getLayout?: (
+    page: JSX.Element,
+    layoutProps: Record<string, unknown>
+  ) => JSX.Element;
+};
+
+export type AppPropsWithLayout<P = Record<string, unknown>> = AppProps<P> & {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Component: PageWithLayout<P> & { theme: string };
 };
