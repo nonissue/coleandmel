53777953bcc2da30d86982362f1abad0cb8e6d8a
Andy Williams <andy@nonissue.org>
2022-03-16 18:05:15 -0600

    fix image path

--- src/components/HeroSeciton/HeroSection.tsx
+++ src/components/HeroSeciton/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Header2 } from "../Header2/Header2";
 const HeroSection = () => {
   return (
     <div>
-      <div className="overflow-hidden relative shadow-sm">
+      <div className="overflow-hidden relative shadow-sm bg-white">
         <div className="mx-auto max-w-7xl">
           <Header2 />
         </div>
@@ -12,7 +12,7 @@ const HeroSection = () => {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             className="object-cover w-full h-72 sm:h-96 md:h-96 lg:w-full lg:h-full"
-            src="./cole-and-mel3.jpg"
+            src="https://coleandmel.com/cole-and-mel3.JPG"
             alt=""
           />
         </div>
