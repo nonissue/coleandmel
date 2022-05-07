88481fdcb025a0eadeb41b9271b2848caccc2f1b
Andy Williams <andy@nonissue.org>
2022-05-06 23:53:03 -0600

    Add gift illustration svg.

---
+++ src/components/Version2/GiftIllustrationSVG.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export const GiftIllustrationSVG = ({ scale }: { scale?: number }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      width={574 * (scale ?? 1)}
+      //   width="574"
+      //   height="533"
+      height={533 * (scale ?? 1)}
+      strokeMiterlimit="2"
+      clipRule="evenodd"
+      viewBox="0 0 2392 2218"
+    >
+      <path fill="none" d="M0 0H2391.11V2217.59H0z"></path>
+      <path fill="#fff" d="M478.889 847.192H1912.219V2068.022H478.889z"></path>
+      <path
+        fill="#3f3d56"
+        fillRule="nonzero"
+        d="M1916.39 2072.19H474.72V843.02h1441.67v1229.17zm-1433.33-8.333h1425v-1212.5h-1425v1212.5z"
+      ></path>
+      <path
+        fill="#e0f2fe"
+        d="M1016.39 1084.69H1374.723V2068.023H1016.39z"
+      ></path>
+      <path
+        fill="#e0f2fe"
+        d="M1016.39 1084.69H1374.723V2068.023H1016.39z"
+      ></path>
+      <path
+        fillOpacity="0.2"
+        fillRule="nonzero"
+        d="M483.055 936.775v198.075l1429.17 91.508-4.212-244.408-1424.95-45.175z"
+      ></path>
+      <path fill="#fff" d="M395.556 734.692H1995.556V1084.692H395.556z"></path>
+      <path
+        fill="#3f3d56"
+        fillRule="nonzero"
+        d="M1999.72 1088.86H391.39V730.526h1608.33v358.334zm-1600-8.334h1591.67V738.86H399.72v341.666z"
+      ></path>
+      <path
+        fill="#e0f2fe"
+        d="M1016.39 701.359H1374.723V1080.526H1016.39z"
+      ></path>
+      <path
+        fill="#e0f2fe"
+        fillRule="nonzero"
+        d="M1157.19 370.76c94.589 140.215 5.468 365.731 5.468 365.731s-242.48-1.818-337.069-142.033C731 454.243 820.12 228.727 820.12 228.727s242.481 1.818 337.07 142.033z"
+      ></path>
+      <path
+        fill="#e0f2fe"
+        fillRule="nonzero"
+        d="M1200.59 370.76c-94.589 140.215-5.469 365.731-5.469 365.731s242.481-1.818 337.07-142.033c94.589-140.215 5.468-365.731 5.468-365.731s-242.48 1.818-337.069 142.033z"
+      ></path>
+      <circle cx="945.556" cy="438.859" r="37.5" fill="#3f3d56"></circle>
+      <circle cx="1170.56" cy="818.025" r="37.5" fill="#3f3d56"></circle>
+      <circle cx="1170.56" cy="1638.86" r="37.5" fill="#3f3d56"></circle>
+      <circle cx="1045.56" cy="576.359" r="25" fill="#3f3d56"></circle>
+      <circle cx="1420.56" cy="363.859" r="25" fill="#3f3d56"></circle>
+      <circle cx="1258.06" cy="951.359" r="25" fill="#3f3d56"></circle>
+      <circle cx="1108.06" cy="1276.36" r="25" fill="#3f3d56"></circle>
+      <circle cx="1237.22" cy="1388.86" r="25" fill="#3f3d56"></circle>
+      <circle cx="1208.06" cy="1859.69" r="25" fill="#3f3d56"></circle>
+    </svg>
+  );
+};
