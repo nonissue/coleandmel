d8f961e2bba6e15874e91e47978ea3642d242a4b
Andy Williams <andy@nonissue.org>
2022-06-29 23:25:12 -0600

    add bar info to considerations

--- src/pages/thespecialday.tsx
+++ src/pages/thespecialday.tsx
@@ -18,14 +18,14 @@ export default function TheSpecialDay() {
         <Nav showLogo={true} />
 
         {/* Hero Copy */}
-        <div className="pt-4 pb-6 md:mt-8 md:pb-0">
+        <div className="pt-4 pb-6 md:mt-0 md:pb-0">
           <div className="mx-auto mt-6 max-w-7xl px-4 md:mt-0 md:px-6">
             <div className="text-center">
-              <h1 className="mx-auto max-w-sm  pb-0 text-4xl tracking-normal text-gray-800 md:pb-6 md:pt-8 md:text-6xl md:leading-tight">
+              <h1 className="mx-auto mt-4 mb-8 max-w-sm py-4 text-4xl tracking-normal text-gray-800 md:py-4 md:text-6xl md:leading-tight">
                 <span className="block pb-1 font-ebgaramond text-base font-[400] uppercase  tracking-widest text-gray-700 md:text-xs">
                   July 21st, 2022
                 </span>
-                <span className="block font-ebgaramond text-3xl font-[400] tracking-tight text-slate-700 md:text-2xl">
+                <span className="block  font-ebgaramond text-3xl font-[500] tracking-tight text-slate-700 md:text-2xl">
                   The Special Day
                 </span>
               </h1>
@@ -39,21 +39,27 @@ export default function TheSpecialDay() {
 
         <article className="my-4 mx-6 max-w-3xl items-center justify-around space-y-1 text-center md:mx-auto">
           <Timeline />
-          <div className="mx-auto max-w-md space-y-4 rounded-none border-2 border-dotted border-slate-300 bg-slate-50 p-6 text-left text-slate-600 shadow-sm md:max-w-xl">
-            <h2 className="text-center font-fraunces text-2xl font-[600] md:text-left">
+          <div className="relative mx-auto max-w-md space-y-4 rounded border border-slate-300 bg-slate-50 p-6 text-left text-slate-700 shadow-sm md:max-w-xl">
+            <h2 className="text-center font-serif  text-2xl font-[500] md:text-left">
               Considerations
             </h2>
-            <ul className="list-disc pl-4 font-ebgaramond">
+            <ul className="list-disc pl-4 font-ebgaramond text-slate-500">
               <li>Our Ceremony and reception will be held outdoors.</li>
               <li>
                 We love your children, but due to space and venue restrictions
                 we cannot accommodate guests under age 18 on our wedding day or
                 any additional guests.
               </li>
-              Please consider carpooling as parking is extremely limited.
+              <li>
+                Please consider carpooling as parking is extremely limited.
+              </li>
               <li>
                 Please join us for an informal brunch at 9:30am on Friday, Jul
                 22, 2022 at the house on the Origin Wines property.
+              </li>
+              <li>
+                Wine will be served during the meal and additional drinks can be
+                purchased at the bar until 10 PM for $2 + tip.
               </li>
               <li>
                 Please
